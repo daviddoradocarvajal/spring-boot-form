3dfec6664e14f2f2f5dcfba85dc7d4af0f263914
CursosDavid <CursosDavid@DESKTOP-40TTL5P>
2023-01-14 12:11:39 +0100

    First Commit

---
+++ src/main/java/com/daviddorado/springboot/form/app/controller/FormController.java
@@ -0,0 +1,61 @@
+package com.daviddorado.springboot.form.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.daviddorado.springboot.form.app.models.domain.Usuario;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class FormController {
+
+	@GetMapping("/form")
+	public String form(Model model) {
+		model.addAttribute("titulo", "Formulario prueba");
+		model.addAttribute("usuario",new Usuario());
+		return "form";
+	}
+
+	/*
+	 * Cuando se envian parámetros desde una vista de formulario el RequestParam lo
+	 * envia desde el atributo name por lo que debe coincidir el nombre del
+	 * parámetro con el nombre del atributo name del formulario
+	 * 
+	 * @RequestParam(name="nombre") donde nombre es el valor del atributo name -> Permite no usar el mismo nombre para el parámetro
+	 * Cuando se valida el Pojo del formulario va como primer parámetro y el BindingResult como segundo
+	 * @ModelAttribute permite cambiar el nombre del objeto para el formulario 
+	 */
+	@PostMapping("/form")
+	public String procesar(@Valid Usuario usuario, BindingResult validationResult ,Model model) // , @RequestParam String username, @RequestParam String
+															// password, @RequestParam String email)
+	{
+		model.addAttribute("titulo", "Resultado formulario");
+		if (validationResult.hasErrors()) {
+			/*Map<String, String> errores = new HashMap<>();
+			validationResult.getFieldErrors().forEach(error -> {
+				errores.put(error.getField(), "El campo ".concat(error.getField()).concat(" ").concat(error.getDefaultMessage()));
+			});
+			model.addAttribute("error",errores);*/
+			return "form";
+		}
+		/*
+		 * Usuario usuario = new Usuario(); 
+		 * usuario.setUsername(username);
+		 * usuario.setPassword(password); 
+		 * usuario.setEmail(email);
+		 */
+		
+		
+		model.addAttribute("usuario", usuario);
+		/*
+		 * model.addAttribute("username", username); 
+		 * model.addAttribute("password", password); 
+		 * model.addAttribute("email", email);
+		 */
+		return "resultado";
+	}
+}
